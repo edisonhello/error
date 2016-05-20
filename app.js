a3468c3a71657bef0f50616d9b6b41df86908e4f
michaelwan <michaelwan0906@hotmail.com>
2016-05-20 19:55:03 +0800

    design

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-var config = require('./config.json');
-
 app.get('/502', function(req, res) {
 	res.sendFile(__dirname+'/502.html', function() {
 		res.end();
